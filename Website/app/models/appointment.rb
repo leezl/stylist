222d7da36a848e69541ec30fa49306779693f460
Liesl Wigand <wolfdrake15@gmail.com>
2014-03-16 18:18:06 -0700

    Updates the validations to include length and regex check; adds several image resources which may be useful as background (probably the white ones not the collored ones)

--- Website/app/models/appointment.rb
+++ Website/app/models/appointment.rb
@@ -2,19 +2,21 @@ class Appointment < ActiveRecord::Base
   attr_accessible :appot, :email, :first_name, :last_name, :message, :phone, :service
 
 # validate First Name - field is filled out, and more than one character less than 15
-  validates :first_name, presence: true
+  validates :first_name, presence: true, length: {minimum: 1, maximum: 15}
 
 # validate Last Name - field is filled out, and more than one character less than 15
 
-  validates :last_name, presence: true
+  validates :last_name, presence: true, length: {minimum: 1, maximum: 15}
 
 # validate Email - field is filled out, and use regex
 
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@(?:[-a-z0-9]+\.)+[a-z]{2,}\z/i, message: "email address invalid " }
 
-#phone - field is filled out, 10 numbers required, auto fill numbers for later 
+#validate phone - field is filled out, 10 numbers required, auto fill numbers for later 
 
-  validates :phone, presence: true
+  validates :phone, presence: true, format: { with: /\A\(?(\d{3})[-\)]?\s?(\d{3})?[-\s]?(\d{4})\z/, message: "phone number invalid" }
+  # a much simpler test, which requires that only numbers be entered
+  #,length: {minimum: 7, maximum: 10}, numericality: { only_integer: true }
 
 
 
